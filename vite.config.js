7034966a87e8acc3d3fa9508c30f717dec7870ae
LubnaHayat05 <lubnahayat8171@gmail.com>
2025-06-09 06:06:59 +0000

    Set correct base for TO-DO-LIST deployment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "TO-DO-LIST",  // e.g., "/my-project/"
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
-    allowedHosts: true
-  }
+  base: "/TO-DO-LIST/", // 👈 this is IMPORTANT
 })
-
